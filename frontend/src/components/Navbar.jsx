44f19a5237d0c47e048a921723238b223af7694b
Thakshila-Prasadanee <dias12436@usci.ruh.ac.lk>
2025-06-17 02:46:03 +0530

    Create Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from 'react'
+import burgerIcon from '../assets/burger.png';
+
+const navItems = [
+    { name: 'About', href: '#about' },
+    { name: 'Classes', href: '#class' },
+    { name: 'Contact', href: '#contact' },
+]
+
+export default function Navbar() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () =>{
+            setIsScrolled(window.scrollY > 10);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+  return (
+    <nav
+        style={{
+            position: 'fixed',
+            width: '100%',
+            zIndex: 40,
+            transition: 'all 0.3s ease',
+            backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        }}
+    >
+      
+        <div 
+            style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+            }}
+        >
+            <a
+                style={{
+                    fontSize: '24px',
+                    fontWeight: '1000',
+                    color: '#f5f5dc',
+                    fontFamily: '"Modak", cursive',
+                    display: 'flex',
+                    alignItems: 'center',
+                    textDecoration: 'none',
+                }}
+                href="#hero"
+            >
+                <img 
+                    src={burgerIcon}
+                    alt="Burger Icon"
+                    style={{
+                        width: '60px',
+                        height: '60px',
+                        margin: '10px',
+                    }}
+                />
+                <span>
+                    LegianPastry
+                </span>
+            </a>
+
+
+            <div
+                style={{
+                    display: 'flex',
+                    gap: '100px',
+                    justifyContent: 'center',
+                    flex: '1',
+                }}
+            >
+                {navItems.map((item, key) => (
+                    <a 
+                        key={key} 
+                        href={item.href}
+                        style={{
+                            fontSize: '20px',
+                            fontWeight: '500',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            color: '#f3ebe2',
+                            transition: 'color 0.3s ease',
+                            textDecoration: 'none',
+                            padding: '10px 20px',
+                            borderRadius: '5px',
+                        }}
+                        onMouseEnter={e => {
+                            e.target.style.color = '#000';
+                            e.target.style.backgroundColor = '#eeb662';
+                        }}
+                        onMouseLeave={e => {
+                            e.target.style.color = '#f3ebe2';
+                            e.target.style.backgroundColor = 'transparent';
+                        }}
+                    >
+                        {item.name}
+                    </a>
+                ))}
+            </div>
+
+            <div>
+                <a
+                    href='#login'
+                    style={{
+                        fontSize: '20px',
+                        fontWeight: '500',
+                        color: '#f3ebe2',
+                        textDecoration: 'none',
+                        alignItems: 'center',
+                        marginRight: '50px',
+                        padding: '10px 20px',
+                        borderRadius: '5px',
+                    }}
+                    onMouseEnter={e => {
+                        e.target.style.color = '#000';
+                        e.target.style.backgroundColor = '#eeb662';
+                    }}
+                    onMouseLeave={e => {
+                        e.target.style.color = '#f3ebe2';
+                        e.target.style.backgroundColor = 'transparent';
+                    }}
+                >
+                    Log In
+                </a>
+            </div>
+        </div>
+
+
+    </nav>
+  )
+}
