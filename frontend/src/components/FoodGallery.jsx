22db2ecc5c755d2352c70a66cd44651e119d5ef7
csheha <chanudishehani22@gmail.com>
2025-07-05 21:16:09 +0530

    update backend links

--- frontend/src/components/FoodGallery.jsx
+++ frontend/src/components/FoodGallery.jsx
@@ -1,74 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/FoodGallery.css';
-import CloseIcon from '@mui/icons-material/Close';
-import axios from 'axios';
-import SpinnerLoader from './SpinnerLoader';
+import React, { useEffect, useState } from "react";
+import "../styles/FoodGallery.css";
+import CloseIcon from "@mui/icons-material/Close";
+import axios from "axios";
+import SpinnerLoader from "./SpinnerLoader";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = `https://legianpastry-production-946e.up.railway.app`;
 
 export default function FoodGallery() {
-    const [model, setModel] = useState(false);
-    const [tempImgSrc, setTempImgSrc] = useState('');
-    const [images, setImages] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState('');
+  const [model, setModel] = useState(false);
+  const [tempImgSrc, setTempImgSrc] = useState("");
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-    // Function to fetch the image
-    useEffect(() => {
-        const fetchImages = async () => {
-            try {
-                const res = await axios.get(`${API_BASE_URL}/images/`);
-                if (Array.isArray(res.data)) {
-                    setImages(res.data);
-                } else {
-                    throw new Error('Invalid response format');
-                }
-            } catch (error) {
-                console.error('Error fetching images:', error);
-                setError('Failed to load images.');
-            } finally {
-                setLoading(false);
-            }
-        };
+  // Function to fetch the image
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const res = await axios.get(`${API_BASE_URL}/images/`);
+        if (Array.isArray(res.data)) {
+          setImages(res.data);
+        } else {
+          throw new Error("Invalid response format");
+        }
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        setError("Failed to load images.");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchImages();
-    }, []);
+    fetchImages();
+  }, []);
 
-    const getImg = (img) => {
-        // Normalize filepath by replacing backslashes with forward slashes
-        const cleanedPath = img.filepath.replace(/\\/g, '/');
-        setTempImgSrc(`${API_BASE_URL}/` + cleanedPath);
-        setModel(true);
-    }
+  const getImg = (img) => {
+    // Normalize filepath by replacing backslashes with forward slashes
+    const cleanedPath = img.filepath.replace(/\\/g, "/");
+    setTempImgSrc(`${API_BASE_URL}/` + cleanedPath);
+    setModel(true);
+  };
 
-    return (
-        <>
-            <div className={model? "model open" : "model"}>
-                <img src={tempImgSrc} />
-                <CloseIcon onClick={() => setModel(false)} />
-            </div>
+  return (
+    <>
+      <div className={model ? "model open" : "model"}>
+        <img src={tempImgSrc} />
+        <CloseIcon onClick={() => setModel(false)} />
+      </div>
 
-            {loading && 
-                <div className="loading">
-                    <SpinnerLoader />
-                </div>
-            }
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            
-            <div className='gallery'>
-                {images.length > 0 && !loading && !error && (
-                    images.map((item, index) => {
-                    return (
-                        <div className='pics' key={index} onClick={() => getImg(item)}>
-                            <img 
-                                src={`${API_BASE_URL}/${item.filepath.replace(/\\/g, '/')}`}
-                                alt={item.title}
-                                style={{width: '100%'}}
-                            />
-                        </div>
-                    )
-                }))}
-            </div>
-        </>
-    )
+      {loading && (
+        <div className="loading">
+          <SpinnerLoader />
+        </div>
+      )}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <div className="gallery">
+        {images.length > 0 &&
+          !loading &&
+          !error &&
+          images.map((item, index) => {
+            return (
+              <div className="pics" key={index} onClick={() => getImg(item)}>
+                <img
+                  src={`${API_BASE_URL}/${item.filepath.replace(/\\/g, "/")}`}
+                  alt={item.title}
+                  style={{ width: "100%" }}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 }
