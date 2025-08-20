63203b03bc50355cbc132aff6679ba3bc9fb86c1
csheha <chanudishehani22@gmail.com>
2025-08-20 18:03:03 +0530

    git ci/cd pipeline test12

--- deployment/push_to_mydocker.sh
+++ deployment/push_to_mydocker.sh
@@ -1,31 +1,28 @@
-DOCKER_HOST=docker.io   # Docker Hub registry
+#!/bin/bash
+
+DOCKER_HOST=docker.io
 IMAGE_APP=legianpastry-app
 IMAGE=legianpastry-react
 
-echo "login to docker repo..."
-#echo "enter password for docker repository "$DOCKER_HOST
-echo "$DOCKER_PASSWORD" | docker login --username=$DOCKER_USERNAME --password-stdin $DOCKER_HOST
+echo "Logging in to Docker Hub..."
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin $DOCKER_HOST
 
-echo "Pushing frontend image to docker repo.."
+echo "Pushing frontend image..."
 docker tag $IMAGE:latest $DOCKER_USERNAME/$IMAGE:latest
-docker tag $IMAGE:latest $DOCKER_USERNAME/$IMAGE:$GITHUB_SHA
+docker tag $IMAGE:latest $DOCKER_USERNAME/$IMAGE:$SHORT_SHA
 
 if [ "$1" == "front" ]; then
   docker push $DOCKER_USERNAME/$IMAGE:latest
-  docker push $DOCKER_USERNAME/$IMAGE:$GITHUB_SHA
+  docker push $DOCKER_USERNAME/$IMAGE:$SHORT_SHA
 fi
 
 echo "Pushing backend image..."
 docker tag $IMAGE_APP:latest $DOCKER_USERNAME/$IMAGE_APP:latest
-docker tag $IMAGE_APP:latest $DOCKER_USERNAME/$IMAGE_APP:$GITHUB_SHA
+docker tag $IMAGE_APP:latest $DOCKER_USERNAME/$IMAGE_APP:$SHORT_SHA
 
 if [ "$2" == "back" ]; then
   docker push $DOCKER_USERNAME/$IMAGE_APP:latest
-  docker push $DOCKER_USERNAME/$IMAGE_APP:$GITHUB_SHA
+  docker push $DOCKER_USERNAME/$IMAGE_APP:$SHORT_SHA
 fi
 
-echo "Completed pushing to Docker Hub "
-echo "================================================================"
-echo "login to hosting site"
-echo "[cd legianpastry/deployment]"
-echo "[run ./build-remote.sh]"
+echo "Completed pushing to Docker Hub ✅"
