dade2928a5aaa7487b6ed3d8fb50dd33bb89c784
csheha <chanudishehani22@gmail.com>
2025-08-20 16:31:43 +0530

    git ci/cd pipeline test4

---
+++ deployment/push_to_mydocker.sh
@@ -0,0 +1,27 @@
+
+IMAGE_APP=legianpastry-app
+IMAGE=legianpastry-react
+
+echo "login to docker repo..."
+#echo "enter password for docker repository "$DOCKER_HOST
+echo "$PASSWORD" | docker login --username=$DOCKER_USER --password-stdin
+
+echo "pushing cshehani/legianpastry-react:latest to docker repo.."
+docker tag $IMAGE:latest $DOCKER_USER/$IMAGE:latest
+
+if [ "$1" == "front" ]; then
+  docker push $DOCKER_USER/$IMAGE:latest
+fi
+
+echo "pushing cshehani/legianpastry-app:latest to docker repo.."
+docker tag $IMAGE_APP:latest $DOCKER_USER/$IMAGE_APP:latest
+
+if [ "$2" == "back" ]; then
+  docker push $DOCKER_USER/$IMAGE_APP:latest
+fi
+
+echo "completed pushing to repo.."
+echo "================================================================"
+echo "login to hosting site"
+echo "[cd legianpastry/deployment]"
+echo "[run ./build-remote.sh]"
