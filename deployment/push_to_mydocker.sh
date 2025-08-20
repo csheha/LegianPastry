b5eb1cd3a3170f642939845cce9111c0a73541ee
csheha <chanudishehani22@gmail.com>
2025-08-20 16:50:08 +0530

    git ci/cd pipeline test8

--- deployment/push_to_mydocker.sh
+++ deployment/push_to_mydocker.sh
@@ -4,20 +4,20 @@ IMAGE=legianpastry-react
 
 echo "login to docker repo..."
 #echo "enter password for docker repository "$DOCKER_HOST
-echo "$PASSWORD" | docker login --username=$DOCKER_USER --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username=$DOCKER_USERNAME --password-stdin
 
 echo "pushing cshehani/legianpastry-react:latest to docker repo.."
-docker tag $IMAGE:latest $DOCKER_USER/$IMAGE:latest
+docker tag $IMAGE:latest $DOCKER_USERNAME/$IMAGE:latest
 
 if [ "$1" == "front" ]; then
-  docker push $DOCKER_USER/$IMAGE:latest
+  docker push $DOCKER_USERNAME/$IMAGE:latest
 fi
 
 echo "pushing cshehani/legianpastry-app:latest to docker repo.."
-docker tag $IMAGE_APP:latest $DOCKER_USER/$IMAGE_APP:latest
+docker tag $IMAGE_APP:latest $DOCKER_USERNAME/$IMAGE_APP:latest
 
 if [ "$2" == "back" ]; then
-  docker push $DOCKER_USER/$IMAGE_APP:latest
+  docker push $DOCKER_USERNAME/$IMAGE_APP:latest
 fi
 
 echo "completed pushing to repo.."
